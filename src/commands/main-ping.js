4e0cc2d1ffb24a896e8b4c472baad02284ca79df
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-14 11:20:31 -0600

    Update main-ping.js

--- src/commands/main-ping.js
+++ src/commands/main-ping.js
@@ -1,19 +1,33 @@
+import { performance } from 'perf_hooks'
+
+const titulo = 'ꕤ *ESTADO DEL SISTEMA*'
+const foot = 'ꕤ Bot funcionando a máxima velocidad'
+
 let handler = async (m, { conn }) => {
-  const latency = Math.floor(Math.random() * (78 - 10 + 1)) + 10
-  const ram = Math.round(process.memoryUsage().rss / 1024 / 1024)
-  const textoFinal = `ꕤ ESTADO DEL SISTEMA
-
-✰ Latencia: *${latency} ms*
-✦ Velocidad: *Muy Rápido*
-ꕤ Estado: *Óptima*
-
-✰ RAM: *${ram} MB*
-❖ Sistema: *${process.platform} ${process.arch}*
-
-ꕤ Bot funcionando a máxima velocidad`
+  const inicio = performance.now()
   
-  const sentMsg = await conn.reply(m.chat, `ꕤ *Calculando...*`, m)
-  await conn.sendMessage(m.chat, { text: textoFinal, edit: sentMsg.key }).catch(() => {})
+  const { key } = await conn.sendMessage(m.chat, { text: 'ꕤ *Calculando...*' }, { quoted: m })
+  
+  const fin = performance.now()
+  const ram = (process.memoryUsage().rss / 1048576).toFixed(0)
+  let lat = Math.round(fin - inicio)
+  
+  let ficticia = lat > 100 ? Math.floor(lat / 10.5) : lat
+  if (ficticia < 5) ficticia = Math.floor(Math.random() * (12 - 5) + 5)
+
+  await conn.sendMessage(m.chat, { 
+    text: `${titulo}
+
+✰ *Latencia:* \`${ficticia} ms\`
+✦ *Velocidad:* \`Rayo\`
+ꕤ *Estado:* \`Óptima\`
+
+✰ *RAM:* \`${ram} MB\`
+❖ *OS:* \`${process.platform}\`
+
+${foot}`, 
+    edit: key 
+  })
 }
 
 handler.help = ['ping']
