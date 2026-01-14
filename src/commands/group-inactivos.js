2eae2b32b4c7219e4dc6c8373b2fc7efbd2daa53
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-14 11:51:58 -0600

    Update group-inactivos.js

--- src/commands/group-inactivos.js
+++ src/commands/group-inactivos.js
@@ -1,6 +1,6 @@
 import { areJidsSameUser } from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn, text, participants, command }) => {
+let handler = async (m, { conn, text, participants, command, usedPrefix }) => {
     const usersData = global.db.data.users
     const args = text.split(' ')
     const days = args[0] && parseInt(args[0]) > 20 ? parseInt(args[0]) : 7
@@ -25,7 +25,7 @@ let handler = async (m, { conn, text, participants, command }) => {
 
         let txt = `❀ Top de comandos de los últimos *${days}* días\n\n`
         txt += items.map((v, i) => `*#${start + i + 1} » ${v.name.replace(/\n/g, ' ')}*\n\t\t» Comandos: \`${v.cmds}\``).join('\n')
-        txt += `\n\n> Página: *${page}* de *${totalPages}*\n> Usa: *.${command} ${days} ${page + 1}*`
+        txt += `\n\n> Página: *${page}* de *${totalPages}*\n> Usa: *${usedPrefix}${command} ${days} ${page + 1}*`
         return await conn.sendMessage(m.chat, { text: txt }, { quoted: m })
     }
 
@@ -43,7 +43,7 @@ let handler = async (m, { conn, text, participants, command }) => {
         txt += `> » Umbral: Menos de 5 comandos\n`
         txt += `> » Total: ${inactivos.length} usuarios\n\n`
         txt += items.map((v, i) => `*#${start + i + 1} » ${v.name.replace(/\n/g, ' ')}*\n\t\t» Comandos: \`${v.cmds}\``).join('\n')
-        txt += `\n\n> Página: *${page}* de *${totalPages}*`
+        txt += `\n\n> Página: *${page}* de *${totalPages}*\n> Usa: *${usedPrefix}${command} ${days} ${page + 1}*`
         
         return await conn.sendMessage(m.chat, { text: txt, mentions: items.map(u => u.id) }, { quoted: m })
     }
