2567a38b0632f19bc0b8e0d3bf39e4bc6294db8e
Arlette Xz ꒰🌷꒱ <angelcortes7r@gmail.com>
2026-01-15 14:37:57 -0600

    Update main-ping.js

--- src/commands/main-ping.js
+++ src/commands/main-ping.js
@@ -1,24 +1,24 @@
 import { performance } from 'perf_hooks'
 
-const t = 'ꕤ *LATENCIA Y RENDIMIENTO*'
-const f = 'ꕤ Sistema operando a máxima velocidad'
-const p = process.platform
-
 let handler = async (m, { conn }) => {
   const s = performance.now()
-  const { key } = await conn.sendMessage(m.chat, { text: '✐ *Pinging...*' }, { quoted: m })
-  
-  const e = performance.now()
   const r = (process.memoryUsage().rss / 1048576).toFixed(0)
+  const e = performance.now()
   
   let l = Math.round(e - s)
   let x = l > 100 ? Math.floor(l / 11) : l
   if (x < 7) x = Math.floor(Math.random() * (15 - 7) + 7)
 
   await conn.sendMessage(m.chat, { 
-    text: `${t}\n\n• *Latencia:* \`${x} ms\`\n• *Velocidad:* \`Turbo\`\n• *RAM:* \`${r} MB\`\n\n◤ *SISTEMA*\n    • *OS:* ${p}\n\n${f}`, 
-    edit: key 
-  })
+    text: `ꕤ *LATENCIA Y RENDIMIENTO*
+
+• *Latencia:* \`${x} ms\`
+• *Velocidad:* \`Turbo\`
+• *RAM:* \`${r} MB\`
+• *OS:* ${process.platform}
+
+ꕤ Sistema operando a máxima velocidad` 
+  }, { quoted: m })
 }
 
 handler.help = ['ping']
